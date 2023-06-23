54b5c14bcc8a428cfc7ab3fc11eb84a785c3cf7f
BaekJongGyu <136159161+BaekJongGyu@users.noreply.github.com>
2023-06-23 18:50:18 +0900

    Add files via upload

--- scripts/donation.js
+++ scripts/donation.js
@@ -3,55 +3,56 @@ class CAppDonation extends CWindowApplication {
     constructor() {
         super(...arguments);
         this.defaultWidth = 300;
-        this.defaultHeight = 200;
+        this.defaultHeight = 230;
         this.appName = "Donation";
-        this.donationScript = `
-    paypal.Buttons({
-        createOrder() {
-            return fetch("http://locahost:3000/create-paypal-order", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                cart: [
-                {
-                    sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
-                    quantity: "YOUR_PRODUCT_QUANTITY",
-                },
-                ],
-            }),
-            })
-            .then((response) => response.json())
-            .then((order) => order.id);
-        },
-        onApprove(data) {
-            return fetch("http://locahost:3000/capture-paypal-order", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                orderID: data.orderID
-            })
-            })
-            .then((response) => response.json())
-            .then((orderData) => {
-                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-                const transaction = orderData.purchase_units[0].payments.captures[0];
-                alert("See console for all available details");
-            });
-        }
-    }).render('#paypal-button-container');`;
     }
     doExecute() {
         super.doExecute();
         this._mainWindow.body.layers.get(0).items.get(0).fill.solidColor = "#101026";
-        let pan = new CPanel(this._mainWindow.body);
-        pan.position.align = EPositionAlign.CLIENT;
         this._mainWindow.body.position.padding.all = 10;
-        pan.controlElement.setAttribute("id", "paypal-button-container");
-        let fn = new Function(this.donationScript);
-        fn();
+        let btn = new CPanel(this._mainWindow.body);
+        btn.resource = "button_gray_gra.control";
+        btn.position.height = 30;
+        btn.position.align = EPositionAlign.TOP;
+        btn.text = "USD 1";
+        btn.onClick = async function () {
+            window.open("https://linuxstiker.cafe24.com/donation1", "popup", "width=500,height=500");
+        };
+        btn = new CPanel(this._mainWindow.body);
+        btn.resource = "button_gray_gra.control";
+        btn.position.margins.top = 5;
+        btn.position.height = 30;
+        btn.position.align = EPositionAlign.TOP;
+        btn.text = "USD 5";
+        btn.onClick = async function () {
+            window.open("https://linuxstiker.cafe24.com/donation5", "popup", "width=500,height=500");
+        };
+        btn = new CPanel(this._mainWindow.body);
+        btn.resource = "button_gray_gra.control";
+        btn.position.margins.top = 5;
+        btn.position.height = 30;
+        btn.position.align = EPositionAlign.TOP;
+        btn.text = "USD 10";
+        btn.onClick = async function () {
+            window.open("https://linuxstiker.cafe24.com/donation10", "popup", "width=500,height=500");
+        };
+        btn = new CPanel(this._mainWindow.body);
+        btn.resource = "button_gray_gra.control";
+        btn.position.margins.top = 5;
+        btn.position.height = 30;
+        btn.position.align = EPositionAlign.TOP;
+        btn.text = "USD 50";
+        btn.onClick = async function () {
+            window.open("https://linuxstiker.cafe24.com/donation50", "popup", "width=500,height=500");
+        };
+        btn = new CPanel(this._mainWindow.body);
+        btn.resource = "button_gray_gra.control";
+        btn.position.margins.top = 5;
+        btn.position.height = 30;
+        btn.position.align = EPositionAlign.TOP;
+        btn.text = "USD 100";
+        btn.onClick = async function () {
+            window.open("https://linuxstiker.cafe24.com/donation100", "popup", "width=500,height=500");
+        };
     }
 }
